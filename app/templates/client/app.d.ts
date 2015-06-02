82d7a8313c761244768e7d58a48589b82e36e7c9
Christophe Feijoo <kubekhrm@gmail.com>
2015-06-02 02:54:40 +0200

    Add Definition file for Client and Shared

--- app/templates/client/app.d.ts
+++ app/templates/client/app.d.ts
@@ -1,1 +1,3 @@
-/// <reference path="../typings/_all.d.ts" />
+/// <reference path="../shared.d.ts" />
+/// <reference path="../typings/angularjs/angular.d.ts" />
+/// <reference path="../typings/socket.io-client/socket.io-client.d.ts" />
