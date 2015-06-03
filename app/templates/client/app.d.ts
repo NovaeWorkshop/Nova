2c3317128568e49ddfa521e52baf2e2be4c77927
Christophe Feijoo <kubekhrm@gmail.com>
2015-06-03 23:38:45 +0200

    Client: Add angular-ui-router definition dependency

--- app/templates/client/app.d.ts
+++ app/templates/client/app.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="../shared.d.ts" />
 /// <reference path="../typings/angularjs/angular.d.ts" />
+/// <reference path="../typings/angular-ui-router/angular-ui-router.d.ts" />
 /// <reference path="../typings/socket.io-client/socket.io-client.d.ts" />
