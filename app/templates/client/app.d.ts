6c66eedcc712236c23948ca83fe5b42f4a97ee16
Christophe FEIJOO <cfeijoo@e2r12p13.42.fr>
2015-06-07 21:29:06 +0200

    Client: Add App namespace and IAuthService Interface

--- app/templates/client/app.d.ts
+++ app/templates/client/app.d.ts
@@ -2,3 +2,15 @@
 /// <reference path="../typings/angularjs/angular.d.ts" />
 /// <reference path="../typings/angular-ui-router/angular-ui-router.d.ts" />
 /// <reference path="../typings/socket.io-client/socket.io-client.d.ts" />
+
+declare module <%= capName %>App {
+
+    interface IAuthService {
+        signup: (user: {}) => ng.IPromise<{}>;
+        login: (user: {}) => ng.IPromise<{}>;
+        logout: () => void;
+        isLogged: () => ng.IPromise<boolean>;
+        getUser: () => ng.IPromise<{}>;
+    }
+
+}
