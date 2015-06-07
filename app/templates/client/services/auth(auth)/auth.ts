fa6c78454bb4f5f5351867fcb85305363c362f7e
Christophe FEIJOO <cfeijoo@e2r12p13.42.fr>
2015-06-07 21:55:05 +0200

    Client: Pass Auth Service to Typescript

--- app/templates/client/services/auth(auth)/auth.ts
+++ app/templates/client/services/auth(auth)/auth.ts
@@ -1,105 +1,96 @@
 /// <reference path="../../app.d.ts" />
 'use strict';
 
-angular.module('<%= appname %>')
-  .service('Auth', function ($rootScope, $cookieStore, $q, $http) {
+module <%= capName %>App.Services.Auth {
 
-    var _user = {};
-    var _ready = $q.defer();
-
-    if ($cookieStore.get('token')) {
-      $http.get('/api/users/me')
-        .then(function (res) {
-          _user = res.data;
-        })
-        .finally(function () {
-          _ready.resolve();
-        });
-    } else {
-      _ready.resolve();
+    interface IAuthCallback extends ng.IHttpPromiseCallbackArg<{}> {
+        data: {
+            user: {};
+            token: string;
+        }
     }
 
-    /**
-     * Signup
-     *
-     * @param user
-     * @returns {promise}
-     */
-    this.signup = function (user) {
-      var deferred = $q.defer();
-      $http.post('/api/users', user)
-        .then(function (res) {
-          _user = res.data.user;
-          $cookieStore.put('token', res.data.token);
-          deferred.resolve();
-        })
-        .catch(function (err) {
-          deferred.reject(err.data);
-        });
-      return deferred.promise;
-    };
+    class AuthService implements IAuthService {
 
-    /**
-     * Login
-     *
-     * @param user
-     * @returns {promise}
-     */
-    this.login = function (user) {
-      var deferred = $q.defer();
-      $http.post('/auth/local', user)
-        .then(function (res) {
-          _user = res.data.user;
-          $cookieStore.put('token', res.data.token);
-          deferred.resolve();
-        })
-        .catch(function (err) {
-          deferred.reject(err.data);
-        });
-      return deferred.promise;
-    };
+        private user;
+        private ready;
 
-    /**
-     * Logout
-     */
-    this.logout = function () {
-      $cookieStore.remove('token');
-      _user = {};
-    };
+        static $inject = ['$cookieStore', '$q', '$http'];
 
-    /**
-     * Check if the user is logged
-     *
-     * @returns {boolean}
-     */
-    this.isLogged = function () {
-      return _user.hasOwnProperty('_id');
-    };
+        constructor(
+            private $cookieStore: ng.cookies.ICookiesService,
+            private $q: ng.IQService,
+            private $http: ng.IHttpService) {
 
-    /**
-     * Check if the user is logged after the ready state
-     *
-     * @returns {Promise}
-     */
-    this.isReadyLogged = function () {
-      var def = $q.defer();
-      _ready.promise.then(function () {
-        if (_user.hasOwnProperty('_id')) {
-          def.resolve();
-        } else {
-          def.reject();
+            var self = this;
+            this.ready = $q.defer();
+
+            if ($cookieStore.get('token')) {
+                $http.get('/api/users/me')
+                    .then(function(res) {
+                        self.user = res.data;
+                    })
+                    .finally(function() {
+                        self.ready.resolve();
+                    });
+            } else
+                self.ready.resolve();
         }
-      });
-      return def.promise;
-    };
 
-    /**
-     * Returns the user
-     *
-     * @returns {object}
-     */
-    this.getUser = function () {
-      return _user;
-    };
+        signup(user) {
+            var self = this;
+            var deferred = this.$q.defer();
 
-  });
+            this.$http.post('/api/users', user)
+                .then(function(res: IAuthCallback) {
+                    self.user = res.data.user;
+                    self.$cookieStore.put('token', res.data.token);
+                    deferred.resolve();
+                })
+                .catch(function(err) {
+                    deferred.reject(err.data);
+                });
+            return deferred.promise;
+        }
+
+        login(user) {
+            var self = this;
+            var deferred = this.$q.defer();
+
+            this.$http.post('/auth/local', user)
+                .then(function(res: IAuthCallback) {
+                    self.user = res.data.user;
+                    self.$cookieStore.put('token', res.data.token);
+                    deferred.resolve();
+                })
+                .catch(function(err) {
+                    deferred.reject(err.data);
+                });
+            return deferred.promise;
+        }
+
+        logout() {
+            this.$cookieStore.remove('token');
+            delete this.user;
+        }
+
+        isLogged() {
+            var self = this;
+            var def = this.$q.defer();
+
+            this.ready.promise.then(function() {
+                if (typeof self.user !== 'undefined')
+                    def.resolve();
+                else
+                    def.reject();
+            });
+            return def.promise;
+        }
+
+        getUser() {
+            return this.user;
+        }
+    }
+
+    angular.module('<%= appname %>').service('Auth', AuthService);
+}
