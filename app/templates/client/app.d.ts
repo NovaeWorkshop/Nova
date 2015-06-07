c527da353765cfa8bc1e5da5d0328a3b59d6208f
Christophe FEIJOO <cfeijoo@e2r12p13.42.fr>
2015-06-07 21:30:47 +0200

    Client: Add typing for Angular Cookies

--- app/templates/client/app.d.ts
+++ app/templates/client/app.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="../shared.d.ts" />
 /// <reference path="../typings/angularjs/angular.d.ts" />
+/// <reference path="../typings/angularjs/angular-cookies.d.ts" />
 /// <reference path="../typings/angular-ui-router/angular-ui-router.d.ts" />
 /// <reference path="../typings/socket.io-client/socket.io-client.d.ts" />
 
