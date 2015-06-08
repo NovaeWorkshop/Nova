75772060b972bb0ce517837435da6cbd77153216
Christophe Feijoo <kubekhrm@gmail.com>
2015-06-08 23:15:35 +0200

    Add NOVA Ascii Logo

--- app/ascii.js
+++ app/ascii.js
@@ -2,15 +2,15 @@
 
 var chalk = require('chalk');
 var version = require('../package.json').version;
-var title =
-  '\n    ' + chalk.red('________                      ') + chalk.yellow('       ______               ') +
-  '\n    ' + chalk.red('___  __ )_____ ______________') + chalk.yellow(' ____  ____  /_____ ________ ') +
-  '\n    ' + chalk.red('__  __  |  __ `/_  __ \\_  __ `') + chalk.yellow('/  / / /_  /_  __ `/_  ___/ ') +
-  '\n    ' + chalk.red('_  /_/ // /_/ /_  / / /  /_/ /') + chalk.yellow('/ /_/ /_  / / /_/ /_  /     ') +
-  '\n    ' + chalk.red('/_____/ \\__,_/ /_/ /_/_\\__, /') + chalk.yellow(' \\__,_/ /_/  \\__,_/ /_/      ') +
-  '\n    ' + chalk.yellow('----------------------') + chalk.red('/____/-----------------------') +
-  '\n    ' + pad(version, 8) + '                      ' + chalk.bgBlack('Light & Fast purposed') +
-  '\n\n';
+
+var title = chalk.green(
+'\n  ooo. .oo.    .ooooo.  oooo    ooo  .oooo.   ' +
+'\n  `888P"Y88b  d88\' `88b  `88.  .8\'  `P  )88b  ' +
+'\n   888   888  888   888   `88..8\'    .oP"888  ' +
+'\n   888   888  888   888    `888\'    d8(  888  ' +
+'\n  o888o o888o `Y8bod8P\'     `8\'     `Y888""8o ') +
+'\n  ' + pad(version, 8) + '                  ' + chalk.bgBlack('Fast Architecture') +
+'\n\n';
 
 function pad (str, nb) {
   return str.length >= nb ? str.substr(0, nb) : str + Array(nb - str.length + 1).join(' ');
