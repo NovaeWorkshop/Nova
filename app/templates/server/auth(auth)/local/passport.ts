c301845aa1dd01b1068701ea8b2aee8ffeb30df4
Christophe Feijoo <kubekhrm@gmail.com>
2015-06-08 23:30:03 +0200

    Server: Format Auth Local Strategy files

--- app/templates/server/auth(auth)/local/passport.ts
+++ app/templates/server/auth(auth)/local/passport.ts
@@ -4,22 +4,24 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-exports.setup = function (User) {
-  passport.use(new LocalStrategy({
-      usernameField: 'email',
-      passwordField: 'password'
+exports.setup = function(User) {
+    passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password'
     },
-    function (email, password, done) {
-      User.findOne({
-        email: email
-      }, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false, { msg: 'email not found' }); }
-        if (!user.authenticate(password)) {
-          return done(null, false, { msg: 'incorrect password' });
+        function(email, password, done) {
+            User.findOne({
+                email: email
+            }, function(err, user) {
+                if (err)
+                    return done(err);
+                if (!user)
+                    return done(null, false, { msg: 'email not found' });
+                if (!user.authenticate(password))
+                    return done(null, false, { msg: 'incorrect password' });
+
+                return done(null, user);
+            });
         }
-        return done(null, user);
-      });
-    }
-  ));
+        ));
 };
