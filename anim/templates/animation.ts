17e0b5d97dae0ab0abfc9e7d2e8ec6a2148b7139
Christophe Feijoo <kubekhrm@gmail.com>
2015-06-10 00:09:33 +0200

    Formatting Animation template

--- anim/templates/animation.ts
+++ anim/templates/animation.ts
@@ -2,44 +2,44 @@
 'use strict';
 
 angular.module('<%= appname %>')
-  .animation('.<%= dashName %>', function () {
-    return {<% if (events.enter) { %>
+    .animation('.<%= dashName %>', function () {
+        return {<% if (events.enter) { %>
 
-      enter: function (element, done) {
+            enter: function (element, done) {
 
-        // animate enter
+                // animate enter
 
-        return function (isCancelled) {
-          if (isCancelled) {
-          }
+                return function (isCancelled) {
+                    if (isCancelled) {
+                    }
+                };
+            }<% }; if (events.leave) { %><%= commaOrNot('leave') %>
+
+            leave: function (element, done) {
+
+                // animate leave
+
+                return function (isCancelled) {
+                    if (isCancelled) {
+                    }
+                };
+            }<% }; if (events.move) { %><%= commaOrNot('move') %>
+
+            move: function (element, done) {
+
+                // animate move
+
+                return function (isCancelled) {
+                    if (isCancelled) {
+                    }
+                };
+            }<% }; if (events.addClass) { %><%= commaOrNot('addClass') %>
+
+            addClass: function (element, className, done) {
+            }<% }; if (events.removeClass) { %><%= commaOrNot('removeClass') %>
+
+            removeClass: function (element, className, done) {
+            }<% }; if (hasEvents) { %>
+            <% } %>
         };
-      }<% }; if (events.leave) { %><%= commaOrNot('leave') %>
-
-      leave: function (element, done) {
-
-        // animate leave
-
-        return function (isCancelled) {
-          if (isCancelled) {
-          }
-        };
-      }<% }; if (events.move) { %><%= commaOrNot('move') %>
-
-      move: function (element, done) {
-
-        // animate move
-
-        return function (isCancelled) {
-          if (isCancelled) {
-          }
-        };
-      }<% }; if (events.addClass) { %><%= commaOrNot('addClass') %>
-
-      addClass: function (element, className, done) {
-      }<% }; if (events.removeClass) { %><%= commaOrNot('removeClass') %>
-
-      removeClass: function (element, className, done) {
-      }<% }; if (hasEvents) { %>
-<% } %>
-    };
-  });
+    });
