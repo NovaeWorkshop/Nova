1f154331456f712d0fd0004c86d48db970b85340
Christophe Feijoo <kubekhrm@gmail.com>
2015-06-11 22:41:23 +0200

    Format Directive Template

--- directive/templates/directive.ts
+++ directive/templates/directive.ts
@@ -2,12 +2,12 @@
 'use strict';
 
 angular.module('<%= appname %>')
-  .directive('<%= camelName %>', function () {
-    return {
-      restrict: 'EA',
-      <% if (needTemplate) { %>templateUrl: 'directives/<%= dashName %>/<%= dashName %>.html',
-      <% } %>link: function (scope, element) {
-        element.text('<%= camelName %> directive');
-      }
-    };
-  });
+    .directive('<%= camelName %>', function () {
+        return {
+            restrict: 'EA',
+                <% if (needTemplate) { %>templateUrl: 'directives/<%= dashName %>/<%= dashName %>.html',
+            <% } %>link: function (scope, element) {
+                element.text('<%= camelName %> directive');
+            }
+        };
+    });
