13ae2c12a8ca9de8fefc55f94426a1c91e40e17a
Christophe Feijoo <kubekhrm@gmail.com>
2015-07-27 13:43:42 +0200

    Update ASCII Art

--- app/ascii.js
+++ app/ascii.js
@@ -4,11 +4,11 @@ var chalk = require('chalk');
 var version = require('../package.json').version;
 
 var title = chalk.green(
-  '\n  nov nov.   .novan;, no.    an: .nova.   ' +
-  '\n  OVAN"VANo aNO\'  `ANo`AN.  nO" `" `NOv  ' +
-  '\n  VAN   NOV NOV    ANO `NO.aN\'  .vA"OVA  ' +
-  '\n  ANO   OVA OVA.  ,NOV  `VAN\'  oV!  VAN  ' +
-  '\n  NOV   VAN  `NOvaNO"    `n\'   `aNOV"`O  ') +
+  '\n  nov,nov.   .novan;, no.    an: .nova.   ' +
+  '\n  OVAN"VANo aNO\'  "ANo`AN.  nO" `"\'`nOv  ' +
+  '\n  VAN   NOV NOV    ANO `NO.aN\'  .vanOVA  ' +
+  '\n  ANO   OVA OVA.  ,NOV  `VAN\'  oV!  vAN  ' +
+  '\n  NOV   VAN  `NOvaNO"    `n\'   `aNOV"NO  ') +
   '\n  ' + pad(version, 8) + '            ' + 'Fast Architecture' +
   '\n\n';
 
